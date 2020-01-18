7a3f5c6358dca5f08f294260d7469f41096002f9
Garrett van Wageningen <ghvanw283@gmail.com>
2020-01-17 22:11:31 -0600

    add rough u8s

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,13 @@ use std::fs::File;
 // https://www.nws.noaa.gov/oh/hrl/misc/xmrg.pdf
 // https://www.nws.noaa.gov/oh/hrl/dmip/2/src/read_xmrg2.c
 
+#[derive(Debug, Copy, Clone)]
 pub enum Endian {
     Little,
     Big
 }
 
+// figure out if there is a way to consolidate some of these with generics. A type that all from_xx_bytes implement
 impl Endian {
     pub fn read_int32<R: Read>(&self, reader: &mut R) -> io::Result<i32> {
         let mut buffer = [0; 4];
@@ -26,7 +28,18 @@ impl Endian {
             Endian::Little => Ok(i32::from_le_bytes(buffer))
         }
     }
+
+    pub fn read_u8<R: Read>(&self, reader: &mut R) -> io::Result<u8> {
+        let mut buffer = [0; 1];
+        reader.read_exact(&mut buffer)?; // need error handling in case not 4 bytes?
+
+        match self {
+            Endian::Big => Ok(u8::from_be_bytes(buffer)),
+            Endian::Little => Ok(u8::from_le_bytes(buffer))
+        }
+    }
 }
+
 
 pub fn read_b_int32<R: Read>(reader: &mut R) -> io::Result<i32> {
 
@@ -35,6 +48,7 @@ pub fn read_b_int32<R: Read>(reader: &mut R) -> io::Result<i32> {
 
     Ok(i32::from_be_bytes(buffer))
 }
+
 
 pub fn get_endian<R: Read>(reader: &mut R) -> io::Result<Endian> {
     let word = read_b_int32(reader);
@@ -47,11 +61,13 @@ pub fn get_endian<R: Read>(reader: &mut R) -> io::Result<Endian> {
     })
 }
 
+
 pub struct ReadBytes {
     count: u64,
     endian: Endian
 }
 
+// figure out if there is a way to consolidate some of these into one using generics
 impl ReadBytes {
 
     pub fn new(count: u64, endian: Endian) -> Self {
@@ -60,27 +76,38 @@ impl ReadBytes {
 
     pub fn read_int32s<R: Read>(&self, reader: &mut R) -> io::Result<Vec<i32>> {
         (0..self.count).map(|_| {
-            // reader.seek(SeekFrom::Current(i))?;
             self.endian.read_int32(reader)
+        })
+        .collect()
+    }
+
+    pub fn read_u8s<R: Read>(&self, reader: &mut R) -> io::Result<Vec<u8>> {
+        (0..self.count).map(|_| {
+            self.endian.read_u8(reader)
         })
         .collect()
     }
 }
 
-// pub fn read_int32s(reader: &mut BufReader<File>) -> io::Result<Vec<i32>> {
-//     // let _ = reader.seek(SeekFrom::Start(4))?;
-
-//     (0..4).map(|_| {
-//         // reader.seek(SeekFrom::Current(i))?;
-//         read_int32(reader)
-//     })
-//     .collect()
-// }
 
 pub fn get_reader(path: &str) -> io::Result<BufReader<File>> {
     let file = File::open(path)?;
     Ok(BufReader::new(file))
 }
+
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    // #[test]
+    // fn as_int32_test() {
+    //     let buff = [1, 1, 1, 1];
+    //     let i = buff.as_int32();
+    //     assert_eq!(i, 16_843_009);
+    // }
+}
+
 
 // pub fn read_xmrg(file_path: &str) -> io::Result<()> {
 //     let file = File::open(file_path)?;
@@ -95,31 +122,8 @@ pub fn get_reader(path: &str) -> io::Result<BufReader<File>> {
 //     Ok(())
 // }
 
-trait ByteReader {
-    fn as_int32(&self) -> i32;
-}
-
-impl ByteReader for [u8; 4] {
-    fn as_int32(&self) -> i32 {
-        i32::from_le_bytes(*self)
-    }
-}
-
-
-
 // impl ByteReader for Vec<u8> {
 //     fn read_int32(&self) -> i32 {
 //         i32::from_le_bytes(*self)
 //     }
 // }
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn as_int32_test() {
-        let buff = [1, 1, 1, 1];
-        let i = buff.as_int32();
-        assert_eq!(i, 16_843_009);
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rexmrg::{ReadBytes, get_endian, get_reader};
+use rexmrg::{ReadBytes, get_endian, get_reader, read_b_int32};
 use std::fs::File;
 use std::io::{Read};
 use std::io;
@@ -11,14 +11,23 @@ fn main() {
 
     let mut reader = get_reader("xmrg0506199516z.gz").unwrap();
     let endian = get_endian(&mut reader).unwrap();
-    // reader.seek(SeekFrom::Current(4)).unwrap();
-    let header_reader = ReadBytes::new(4, endian);
+    let header_bytes = ReadBytes::new(4, endian);
 
-    println!("the header is {:?}", header_reader.read_int32s(&mut reader).unwrap());
+    println!("the header is {:?}", header_bytes.read_int32s(&mut reader).unwrap());
+
+    // reader.seek(SeekFrom::Current(4)).unwrap();
+
+    let num_header2_bytes = endian.read_int32(&mut reader).unwrap();
+    // let num_header2_bytes = read_b_int32(&mut reader).unwrap();
+    let header2_bytes = ReadBytes::new(num_header2_bytes as u64, endian);
+
+    println!("num header2 bytes {}", num_header2_bytes);
+
+    println!("the header 2 is {:?}", header2_bytes.read_u8s(&mut reader).unwrap());
     
     println!("now for the tester");
 
-    tester("xmrg0506199516z.gz", 32).unwrap();
+    // tester("xmrg0506199516z.gz", 32).unwrap();
 
     println!("Fin ...");
 }
